88e7a1c595f223dee9b5220bacd86c22c26ee95b
klandco <92538262+klandco@users.noreply.github.com>
2023-10-31 15:07:30 -0400

    Update script.js
    icon size2

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ $.getJSON("src/Jp2gProjectPointsNew.geojson", function (data){
   var iconStyle = L.icon({
     iconUrl: "src/redcircle.png",
     iconRetinaUrl: 'src/redcircle.png',
-    iconSize: [18, 18]
+    iconSize: [2, 2]
   });
   var geoJsonLayer = L.geoJson(data, {
     pointToLayer: function( feature, latlng) {
